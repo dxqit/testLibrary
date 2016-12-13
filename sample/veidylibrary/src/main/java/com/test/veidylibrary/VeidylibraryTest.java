bc5485fd993354f77bde21600e301e8c17c8b66f
dxqit <dxqit@163.com>
2016-12-13 17:09:54 +0800

    test

---
+++ sample/veidylibrary/src/main/java/com/test/veidylibrary/VeidylibraryTest.java
@@ -0,0 +1,17 @@
+package com.test.veidylibrary;
+
+/**
+ * 项目名称：HuixinDoctor
+ * 类名称：com.test.veidylibrary
+ * 类描述：
+ * 创建人：dxqit
+ * 创建时间：2016/12/13 下午5:07
+ *
+ * @version 1.0.0
+ */
+public class VeidyLibraryTest {
+
+    private void getTest(){
+
+    }
+}
